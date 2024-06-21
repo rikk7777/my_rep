841078836f658782efff383d7a45682d976ffa28
Erika Kravtsova <rikkitikkit777@gmail.com>
2024-06-21 21:56:42 +0300

    another some changes

--- testpy.py
+++ testpy.py
@@ -1,1 +1,14 @@
-print('Hy')
+import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+api_url = "http://api.open-notify.org/iss-now.json"
+response = requests.get(api_url)
+coordinates = response.json().get("iss_position")
+formatted_coordinates = f"{coordinates['longitude']}%2C{coordinates['latitude']}"
+map_url = f"https://yandex.com/maps/?ll={formatted_coordinates}&z=10"
+with webdriver.Chrome() as driver:
+    driver.get(map_url)
+    time.sleep(30)
+
